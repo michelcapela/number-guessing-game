8eaa38c581fc411d9261418c24150d296b40e184
Michel Capela <michel.capela@outlook.com>
2024-10-15 15:28:06 -0300

    feat: game logic

--- number-guessing-game/Program.cs
+++ number-guessing-game/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("You have to try to choose the number I chose, between 1 to 10!");
+
+
+
+bool isCorrectGuess = false;
+
+Random random = new Random();
+
+while (!isCorrectGuess)
+{
+
+    int number = random.Next(1, 11);
+
+    Console.WriteLine("Guess a number:");
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess > number) { Console.WriteLine("Your guess is too right"); }
+    else if (guess < number) { Console.WriteLine("Your guess is too low"); }
+    else { Console.WriteLine("Correct!"); isCorrectGuess = true; }
+
+
+}
+
+Console.ReadKey();
